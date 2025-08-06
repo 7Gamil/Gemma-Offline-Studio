0345269b7a88b820fc426d93472a5d0c3f11b5fd
Peter Magdy <56898701+7Gamil@users.noreply.github.com>
2025-08-07 01:23:25 +0300

    Add frontend files

---
+++ frontend/app/components/home.tsx
@@ -0,0 +1,259 @@
+"use client";
+
+require("../polyfill");
+
+import styles from "./home.module.scss";
+
+import log from "loglevel";
+import dynamic from "next/dynamic";
+import { useState, useEffect, useRef } from "react";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import LoadingIcon from "../icons/three-dots.svg";
+
+import Locale from "../locales";
+import { getCSSVar, useMobileScreen } from "../utils";
+import { DEFAULT_MODELS, Path, SlotID } from "../constant";
+import { ErrorBoundary } from "./error";
+import { getISOLang, getLang } from "../locales";
+import { SideBar } from "./sidebar";
+import { useAppConfig } from "../store/config";
+import { ModelClient, useChatStore } from "../store";
+import { LocalAPIContext } from "../context";
+import { LocalApi } from "../client/local";
+
+export function Loading(props: { noLogo?: boolean }) {
+  return (
+    <div className={styles["loading-content"] + " no-dark"}>
+      <LoadingIcon />
+    </div>
+  );
+}
+
+export function ErrorScreen(props: { message: string }) {
+  return (
+    <div className={styles["error-screen"] + " no-dark"}>
+      <p>{props.message}</p>
+    </div>
+  );
+}
+
+const Settings = dynamic(async () => (await import("./settings")).Settings, {
+  loading: () => <Loading noLogo />,
+});
+
+const Chat = dynamic(async () => (await import("./chat")).Chat, {
+  loading: () => <Loading noLogo />,
+});
+
+const TemplatePage = dynamic(
+  async () => (await import("./template")).TemplatePage,
+  {
+    loading: () => <Loading noLogo />,
+  },
+);
+
+export function useSwitchTheme() {
+  const config = useAppConfig();
+
+  useEffect(() => {
+    document.body.classList.remove("light");
+    document.body.classList.remove("dark");
+
+    if (config.theme === "dark") {
+      document.body.classList.add("dark");
+    } else if (config.theme === "light") {
+      document.body.classList.add("light");
+    }
+
+    const metaDescriptionDark = document.querySelector(
+      'meta[name="theme-color"][media*="dark"]',
+    );
+    const metaDescriptionLight = document.querySelector(
+      'meta[name="theme-color"][media*="light"]',
+    );
+
+    if (config.theme === "auto") {
+      metaDescriptionDark?.setAttribute("content", "#151515");
+      metaDescriptionLight?.setAttribute("content", "#fafafa");
+    } else {
+      const themeColor = getCSSVar("--theme-color");
+      metaDescriptionDark?.setAttribute("content", themeColor);
+      metaDescriptionLight?.setAttribute("content", themeColor);
+    }
+  }, [config.theme]);
+}
+
+function useHtmlLang() {
+  useEffect(() => {
+    const lang = getISOLang();
+    const htmlLang = document.documentElement.lang;
+
+    if (lang !== htmlLang) {
+      document.documentElement.lang = lang;
+    }
+  }, []);
+}
+
+const useHasHydrated = () => {
+  const [hasHydrated, setHasHydrated] = useState<boolean>(false);
+
+  useEffect(() => {
+    setHasHydrated(true);
+  }, []);
+
+  return hasHydrated;
+};
+
+const loadAsyncFonts = () => {
+  const linkEl = document.createElement("link");
+  linkEl.rel = "stylesheet";
+  linkEl.href = "/fonts/font.css";
+  document.head.appendChild(linkEl);
+};
+
+function Screen() {
+  const config = useAppConfig();
+  const location = useLocation();
+  const isHome = location.pathname === Path.Home;
+  const isMobileScreen = useMobileScreen();
+  const shouldTightBorder = config.tightBorder && !isMobileScreen;
+
+  useEffect(() => {
+    loadAsyncFonts();
+  }, []);
+
+  return (
+    <div
+      className={
+        styles.container +
+        ` ${shouldTightBorder ? styles["tight-container"] : styles.container} ${
+          getLang() === "ar" ? styles["rtl-screen"] : ""
+        }`
+      }
+    >
+      <>
+        <SideBar className={isHome ? styles["sidebar-show"] : ""} />
+
+        <div className={styles["window-content"]} id={SlotID.AppBody}>
+          <Routes>
+            <Route path={Path.Home} element={<Chat />} />
+            <Route path={Path.Templates} element={<TemplatePage />} />
+            <Route path={Path.Chat} element={<Chat />} />
+            <Route path={Path.Settings} element={<Settings />} />
+          </Routes>
+        </div>
+      </>
+    </div>
+  );
+}
+
+const useLoadUrlParam = () => {
+  const config = useAppConfig();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    let modelConfig: any = {
+      model: params.get("model"),
+      temperature: params.has("temperature")
+        ? parseFloat(params.get("temperature")!)
+        : null,
+      top_p: params.has("top_p") ? parseFloat(params.get("top_p")!) : null,
+      max_tokens: params.has("max_tokens")
+        ? parseInt(params.get("max_tokens")!)
+        : null,
+      presence_penalty: params.has("presence_penalty")
+        ? parseFloat(params.get("presence_penalty")!)
+        : null,
+      frequency_penalty: params.has("frequency_penalty")
+        ? parseFloat(params.get("frequency_penalty")!)
+        : null,
+    };
+    Object.keys(modelConfig).forEach((key) => {
+      // If the value of the key is null, delete the key
+      if (modelConfig[key] === null) {
+        delete modelConfig[key];
+      }
+    });
+    if (Object.keys(modelConfig).length > 0) {
+      log.info("Loaded model config from URL params", modelConfig);
+      config.updateModelConfig(modelConfig);
+    }
+  }, []);
+};
+
+const useStopStreamingMessages = () => {
+  const chatStore = useChatStore();
+
+  // Clean up bad chat messages due to refresh during generating
+  useEffect(() => {
+    chatStore.stopStreaming();
+  }, []);
+};
+
+const useLogLevel = () => {
+  const config = useAppConfig();
+
+  // Update log level once app config loads
+  useEffect(() => {
+    log.setLevel(config.logLevel as any);
+  }, [config.logLevel]);
+};
+
+const useModels = (
+  localApi: LocalApi | undefined,
+) => {
+  const config = useAppConfig();
+
+  useEffect(() => {
+    if (config.modelClientType == ModelClient.LOCAL_API) {
+      if (localApi) {
+        localApi.models().then((models) => {
+          config.setModels(models);
+        });
+      }
+    }
+  }, [config.modelClientType, localApi]);
+};
+
+const useLocalApi = () => {
+  const [localApi, setLocalApi] = useState<LocalApi | undefined>(undefined);
+
+  useEffect(() => {
+    setLocalApi(new LocalApi());
+  }, []);
+
+  return localApi;
+};
+
+
+export function Home() {
+  const hasHydrated = useHasHydrated();
+  const localApi = useLocalApi();
+
+  useSwitchTheme();
+  useHtmlLang();
+  useLoadUrlParam();
+  useStopStreamingMessages();
+  useModels(localApi);
+  useLogLevel();
+
+  if (!hasHydrated || !localApi) {
+    return <Loading />;
+  }
+
+  return (
+    <ErrorBoundary>
+      <Router>
+        <LocalAPIContext.Provider value={localApi}>
+          <Screen />
+        </LocalAPIContext.Provider>
+      </Router>
+    </ErrorBoundary>
+  );
+}
